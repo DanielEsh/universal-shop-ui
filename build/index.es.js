e4d20953c2af6718a24bf758a58b776bf4e18bb0
Daniel <daniilterawars@gmail.com>
2022-02-28 20:31:48 +0300

    initial commit

---
+++ build/index.es.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+var Component = function () {
+    return (React.createElement("div", null, "Component"));
+};
+
+export { Component };
+//# sourceMappingURL=index.es.js.map
